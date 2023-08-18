8f6f045cf87d517fc1b01a40cd87808a4c2d7e91
James Oswald <jamesoswald111@gmail.com>
2023-08-18 02:03:23 -0400

    CV for identifying dots

--- generateProblem.py
+++ generateProblem.py
@@ -45,50 +45,42 @@ def problemGen(n : int, m : int, flows: dict[str, tuple[tuple[int, int], tuple[i
 
 )'''
 
+def genProblemFile(n : int, m : int, flows: dict[str, tuple[tuple[int, int], tuple[int, int]]]):
+    with open(f"flowproblem{n}x{m}.pddl", "w") as file:
+        file.write(problemGen(n, m, flows))
+
+#Some hand coded problems
 if __name__ == "__main__":
-    with open("flowproblem3x1.pddl", "w") as file:
-        file.write(problemGen(3, 1, {
-            "red": ((0,0), (2, 0)),
-        }))
+    genProblemFile(3, 1, {
+        "red": ((0,0), (2, 0))
+    })
     
-    with open("flowproblem5x5.pddl", "w") as file:
-        file.write(problemGen(5, 5, {
-            "red": ((0,0), (1, 4)),
-            "green": ((1,3), (2, 0)),
-            "blue": ((2, 1), (2, 4)),
-            "yellow": ((3,3), (4,0)),
-            "orange": ((3,4), (4,1)),
-        }))
+    genProblemFile(5, 5, {
+        "red": ((0,0), (1, 4)),
+        "green": ((1,3), (2, 0)),
+        "blue": ((2, 1), (2, 4)),
+        "yellow": ((3,3), (4,0)),
+        "orange": ((3,4), (4,1)),
+    })
 
-    with open("flowproblem6x6.pddl", "w") as file:
-        file.write(problemGen(8, 8, {
-            "orange": ((1, 1),(7,5)),
-            "yellow": ((0,7), (3, 4)),
-            "blue" : ((4,5), (7, 7)),
-            "green": ((7,4), (7,6)),
-            "red" : ((1, 5), (5,3)),
-        }))
+    genProblemFile(8, 8, {
+        "orange": ((1, 1),(7,5)),
+        "yellow": ((0,7), (3, 4)),
+        "blue" : ((4,5), (7, 7)),
+        "green": ((7,4), (7,6)),
+        "red" : ((1, 5), (5,3)),
+    })
 
-    with open("flowproblem8x8.pddl", "w") as file:
-        file.write(problemGen(8, 8, {
-            "orange": ((1, 1),(7,5)),
-            "yellow": ((0,7), (3, 4)),
-            "blue" : ((4,5), (7, 7)),
-            "green": ((7,4), (7,6)),
-            "red" : ((1, 5), (5,3)),
-        }))
-
-    with open("flowproblem15x18.pddl", "w") as file:
-        file.write(problemGen(15, 18, {
-            "yellow": ((0,1), (7, 17)),
-            "cyan": ((2,2), (7, 9)),
-            "rust": ((4,1), (13, 14)),
-            "green": ((0,9), (12,5)),
-            "pink": ((6,6), (12,15)),
-            "white": ((7,7),(5,13)),
-            "orange": ((8, 10),(6,17)),
-            "gray" : ((2,15),(10,5)),
-            "blue" : ((11,5), (11, 14)),
-            "purple" : ((11,6),(13,16)),
-            "red" : ((4, 16), (12,16)),
-        }))
+    genProblemFile(15, 18, {
+        "yellow": ((0,1), (7, 17)),
+        "cyan": ((2,2), (7, 9)),
+        "rust": ((4,1), (13, 14)),
+        "green": ((0,9), (12,5)),
+        "pink": ((6,6), (12,15)),
+        "white": ((7,7),(5,13)),
+        "orange": ((8, 10),(6,17)),
+        "gray" : ((2,15),(10,5)),
+        "blue" : ((11,5), (11, 14)),
+        "purple" : ((11,6),(13,16)),
+        "red" : ((4, 16), (12,16)),
+    })
